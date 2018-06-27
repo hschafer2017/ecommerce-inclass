abc7077694bc4735659aba194f1a04a6f34f7de2
Haley Schafer <hschafer@umich.edu>
2018-06-27 11:19:44 +0000

    cart bug fix and grand total

--- cart/views.py
+++ cart/views.py
@@ -4,35 +4,40 @@ from products.models import Product
 # Create your views here - CART.
 def view_cart(request):
     cart = request.session.get('cart', {})
-    # return { 'cart_items': cart_items, 'total': total }
-    
-    
+    totals = 0
     products = []
     for p in cart:
-        phone = get_object_or_404(Product, pk=p)
+        product = get_object_or_404(Product, pk=p)
         products.append({
-            'product' : phone,
+            'product' : product,
             'quantity': cart[p],
-            'price': phone.price,
-            'total': (cart[p] * phone.price)
+            'price': product.price,
+            'image': product.image,
+            'total': (cart[p] * product.price)
         })
-    
-    return render(request, 'cart/cart.html', {'cart': products})
+        totals += cart[p] * product.price
+        
+    return render(request, 'cart/cart.html', {'cart': products, 'totals': totals})
+
+
 
 def add_to_cart(request):
 
     # Get the product we're adding
-    id = int(request.POST['product_id'])
+    id = request.POST['product_id']
     products = get_object_or_404(Product, pk=id)
 
     # Get the current cart
     cart = request.session.get('cart', {})
+    print(cart)
     
     # Update the cart
     cart[id] = cart.get(id, 0) + 1
+    print(cart[id])
     
     # Save the Cart back to the session
     request.session['cart'] = cart
+    print(cart)
     
     # Redirect somewhere
     return HttpResponse(cart[id])
