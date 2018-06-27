ad3a58f4d5433d29c332791963349902e6561997
Haley Schafer <hschafer@umich.edu>
2018-06-27 13:49:49 +0000

    Remove item from cart

--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404, HttpResponse
+from django.shortcuts import render, get_object_or_404, HttpResponse, redirect
 from products.models import Product
 
 # Create your views here - CART.
@@ -6,8 +6,11 @@ def view_cart(request):
     cart = request.session.get('cart', {})
     totals = 0
     products = []
+    
     for p in cart:
+        print(p)
         product = get_object_or_404(Product, pk=p)
+        print(product)
         products.append({
             'product' : product,
             'quantity': cart[p],
@@ -16,8 +19,19 @@ def view_cart(request):
             'total': (cart[p] * product.price)
         })
         totals += cart[p] * product.price
-        
+
+    
     return render(request, 'cart/cart.html', {'cart': products, 'totals': totals})
+
+def remove_item(request): 
+    
+    id = request.POST['product_id']
+    products = get_object_or_404(Product, pk=id)
+    cart = request.session.get('cart', {})
+    del cart[id]
+    request.session['cart'] = cart
+    return redirect('view_cart')
+
 
 
 
@@ -29,15 +43,12 @@ def add_to_cart(request):
 
     # Get the current cart
     cart = request.session.get('cart', {})
-    print(cart)
     
     # Update the cart
     cart[id] = cart.get(id, 0) + 1
-    print(cart[id])
     
     # Save the Cart back to the session
     request.session['cart'] = cart
-    print(cart)
     
     # Redirect somewhere
     return HttpResponse(cart[id])
