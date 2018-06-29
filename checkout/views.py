0349c1d327a1238cf72b2159fbcd632fe12faf77
Haley Schafer <hschafer@umich.edu>
2018-06-29 10:59:05 +0000

    stripe setup and working

--- checkout/views.py
+++ checkout/views.py
@@ -7,6 +7,7 @@ from cart.utils import get_cart_items_and_total
 from django.utils import timezone
 from .models import OrderLineItem
 from django.contrib import messages
+from .utils import save_order_items, charge_card, send_confirmation_email
 import stripe
 from django.conf import settings
 
@@ -17,31 +18,41 @@ def checkout(request):
         order_form = OrderForm(request.POST)    
         payment_form = MakePaymentForm(request.POST)
         
-        if order_form.is_valid():
+        if order_form.is_valid() and payment_form.is_valid():
             # Save The Order
-            order = order_form.save()
-           
+            order = order_form.save(commit=False)
+            order.date = timezone.now()
+            order.save()
         
             # Save the Order Line Items
             cart = request.session.get('cart', {})
-            for product_id, quantity in cart.items(): 
-                line_item = OrderLineItem()
-                
-                line_item.order = order
-                line_item.products = get_object_or_404(Product, pk=product_id)
-                
-                line_item.quantity = quantity
-                line_item.save()
+            save_order_items(order, cart)
+            
+            # Charge the Card
+            items_and_total = get_cart_items_and_total(cart)
+            total = items_and_total['totals']
+            stripe_token=payment_form.cleaned_data['stripe_id']
+
+            try:
+                customer = charge_card(stripe_token, total)
+            except stripe.error.CardError:
+                messages.error(request, "Your card was declined!")
+
+            if customer.paid:
+                messages.error(request, "You have successfully paid")
+
+                # Send Email
+                send_confirmation_email(request.user.email, request.user, items_and_total)
         
-            # Clear the Cart
-            del request.session['cart']
-            return HttpResponse("Checkout completed")
+                # Clear the Cart
+                del request.session['cart']
+                return redirect("home")
     else:
-        cart = request.session.get('cart', {})
         order_form = OrderForm()
         payment_form = MakePaymentForm()
-        context = get_cart_items_and_total(cart)
-        forms = {'order_form': order_form, 'payment_form': payment_form, 'publishable': settings.STRIPE_PUBLISHABLE}
-        context.update(forms)
+        context = {'order_form': order_form, 'payment_form': payment_form, 'publishable': settings.STRIPE_PUBLISHABLE }
+        cart = request.session.get('cart', {})
+        cart_items_and_total = get_cart_items_and_total(cart)
+        context.update(cart_items_and_total)
     
     return render(request, "checkout/checkout.html", context)
